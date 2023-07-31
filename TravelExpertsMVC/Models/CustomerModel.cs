9bb25cca050b783b82ea1e54f9852406904ca724
amkatsendjargal <amkatsendjargal@AMKATSENDJA1FDF>
2023-07-30 22:16:32 -0600

    Login and Logout added. CustomerModel added. Register/Login on left side of navbar

---
+++ TravelExpertsMVC/Models/CustomerModel.cs
@@ -0,0 +1,27 @@
+﻿using TravelExpertsData;
+
+namespace TravelExpertsMVC.Models
+{
+    public class CustomerModel
+    {
+        private static List<Customer> GetCustomers(TravelExpertsContext db)
+        {
+            List<Customer> customers = db.Customers.ToList();
+            return customers;
+        }
+
+        // Will use for registering
+        public static void Add(TravelExpertsContext db, Customer customer)
+        {
+            db.Customers.Add(customer);
+            db.SaveChanges();
+        }
+
+        public static Customer Authenticate(TravelExpertsContext db, string userId, string userPwd)
+        {
+            var customer = GetCustomers(db).SingleOrDefault(cust => cust.UserId == userId &&
+                                                                    cust.UserPwd == userPwd);
+            return customer;
+        }
+    }
+}
