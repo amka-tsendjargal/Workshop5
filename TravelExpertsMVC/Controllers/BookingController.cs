c0ae7d8b7e2d5f85be6da76ecea72de2bbae67cd
Grayson Germsheid <grayson.germsheid@edu.sait.ca>
2023-08-02 11:12:01 -0600

    Added comments for BookingHistory; Fixed error checking in BookingController

--- TravelExpertsMVC/Controllers/BookingController.cs
+++ TravelExpertsMVC/Controllers/BookingController.cs
@@ -5,29 +5,46 @@ using TravelExpertsMVC.Models;
 
 namespace TravelExpertsMVC.Controllers
 {
+    /// <summary>
+    /// This class is a controller for pages displaying Bookings
+    /// </summary>
     public class BookingController : Controller
     {
+        // The TravelExperts database context
         private TravelExpertsContext _dbContext;
 
+        // Constructor for dependency injection
         public BookingController(TravelExpertsContext dbContext)
         {
             _dbContext = dbContext;
         }
 
+        // The BookingHistory page, only valid for logged-in users
+        // this page displays a list of the past bookings that a customer has made
         [Authorize]
         public IActionResult BookingHistory()
         {
             try
             {
-                int customerID = (int)HttpContext.Session.GetInt32("CurrentCustomer")!;
-                var Model = BookingManager.GetCustomerBookings(_dbContext, customerID);
+                // Get customer ID from session
+                int? customerID = HttpContext.Session.GetInt32("CurrentCustomer");
+
+                if (customerID == null)
+                {
+                    TempData["ErrorMessage"] = "Your session has expired.";
+                    return RedirectToAction("Logout", "Customer");
+                }
+
+                // Model is a BookingViewModel (only relevant data from booking, pre-
+                // formatted strings for displaying)
+                var Model = BookingManager.GetCustomerBookings(_dbContext, (int)customerID);
 
                 return View(Model);
             }
-            catch
+            catch // database error
             {
-                TempData["ErrorMessage"] = "There was an error accessing your account information.";
-                return RedirectToAction("Logout", "Customer");
+                TempData["ErrorMessage"] = "There was an error accessing your booking history.";
+                return RedirectToAction("Index", "Home");
             }
         }
     }
